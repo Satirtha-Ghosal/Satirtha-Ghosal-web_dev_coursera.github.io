74de185df254b8f1e0d017070ff705174e90c0a5
Satirtha-Ghosal <138100530+Satirtha-Ghosal@users.noreply.github.com>
2023-07-16 19:43:33 +0530

    Add files via upload

---
+++ module 4/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var hello = {};
+  var speakWord = "Hello";
+  hello.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.hello = hello;
+})(window);
